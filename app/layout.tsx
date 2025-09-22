4ae61c78b8a6597ba11d9aa861161c0f576f6bf4
fumblerill <shidman01@yandex.ru>
2025-09-23 09:37:46 +1000

    fix: исправление имени фавикона

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export const metadata: Metadata = {
   title: "Questly",
   icons: {
-    icon: "/images.png",
+    icon: "/favicon.png",
   },
   description: "Генератор случайных приключений",
 };
